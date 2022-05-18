a3002dd7a74683826f6780f76ef9fff6db9151e0
ChaniAsh <99870470+ChaniAsh@users.noreply.github.com>
2022-05-18 21:54:47 +0300

    Add files via upload

---
+++ Warning.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Warning(){
+    return(
+    <><div id='warningLogo'></div>
+    <div id="object" class="slideUp"></div>
+    
+    </>
+    )
+}
