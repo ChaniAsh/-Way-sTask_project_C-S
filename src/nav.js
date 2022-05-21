ed2bb5ad188e2a07941e52e48d4781d9a3768f97
ChaniAsh <99870470+ChaniAsh@users.noreply.github.com>
2022-05-22 00:53:54 +0300

    Add files via upload

--- src/nav.js
+++ src/nav.js
@@ -1,9 +1,9 @@
 import {Link} from 'react-router-dom'
 export default function NavComp(){
     return(
-        <div className='nav'>
-        <nav>
-        <Link to='..'><img src='/public/finalogo.png'></img></Link>
+        <div >
+        <nav >
+        <Link to='..'><div id='logoPage'></div></Link>
         </nav>
         </div>
     )
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,10 @@ import NewTask from './Components/NewTask.js'
 import SaveTask from './Components/SaveTask'
 import Position from './Components/Position'
 import EditPosition from './Components/EditPosition.js'
+import Warning from './Components/Warning'
+import Map from './Components/GoogleMaps' 
+import BGroup from './Components/ButtonGroupTask'
+import EditTask from './Components/EditTask'
 
 export default ()=>{
 
@@ -14,6 +18,10 @@ export default ()=>{
         <Route path='saveTask' element={<SaveTask/>}/>
         <Route path='position' element={<Position/>}/>
         <Route path='editPosition' element={<EditPosition/>}/>
+        <Route path='warning' element={<Warning/>}/>
+        <Route path='map' element={<Map/>}/>
+        <Route path='group' element={<BGroup/>}/>
+        <Route path='editTask' element={<EditTask/>}/>
     </Routes>
     )
     
